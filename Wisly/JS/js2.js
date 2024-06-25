c3e5bfa5431cbfd8e25c6b8cd5fcee44bf4a499f
Yash Vardhan Singh <129684526+yashclouded@users.noreply.github.com>
2024-06-26 00:45:39 +0530

    Main File upload

---
+++ Wisly/JS/js2.js
@@ -0,0 +1,11 @@
+let a = document.querySelector("chat_gpt_image")
+const b = {
+Text1:"Chat GPT",
+Text2:"Chat GPT",
+Text3:"Chat GPT"
+}
+function b() {
+    document.getElementById$(imageno).innerHTML = b.Text1
+
+}
+document.addEventListener("mouseover", b())
